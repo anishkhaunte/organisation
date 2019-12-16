4ea4fa5df169b5ac74c06d6f4931fd2cb11fc875
vishnu_khaunte <vishnu.khaunte@ril.com>
2019-12-16 14:44:04 +0530

    indexes added

--- db/schemas/relation.schema.js
+++ db/schemas/relation.schema.js
@@ -1,12 +1,7 @@
 function loadSchema(Sequelize,DataTypes){
 
     var relation = {
-        '_id': {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
+        
         'organization_id':{
             type: DataTypes.INTEGER,
             model:'Organizations',
@@ -31,7 +26,12 @@ function loadSchema(Sequelize,DataTypes){
             field: 'updated_at' 
         }
     };
-    var Relation = Sequelize.define('Relations',relation,{freezeTableName:true, timestamps:false, underscored:true});
+    var indexes = [
+        {
+            fields: ['organization_id', 'parent_id']
+        }
+    ];
+    var Relation = Sequelize.define('Relations',relation,{freezeTableName:true, timestamps:false, underscored:true, indexes:indexes});
     
     Relation.hasMany(Sequelize.models.Relations,{as: 'parents',foreignKey:'organization_id', sourceKey:'parent_id',constraints: false});
     Relation.hasMany(Sequelize.models.Relations,{as: 'kids',foreignKey:'parent_id', sourceKey:'organization_id', constraints: false});    
